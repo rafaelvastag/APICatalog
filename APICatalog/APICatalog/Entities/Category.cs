ccd6bd7f306d7e1a8438d93821e0c64b3e6000e2
Rafael Vastag <rgvastag@gmail.com>
2021-11-15 17:56:58 -0300

    first commit

---
+++ APICatalog/APICatalog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalog.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public string imageUrl { get; set; }
+    }
+}
---
+++ APICatalog/APICatalog/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalog.Entities
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImagemUrl { get; set; }
+        public float  Inventory { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
