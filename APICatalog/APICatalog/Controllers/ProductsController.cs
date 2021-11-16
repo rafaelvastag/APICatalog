32274a4f809386d37b2e43b40aa2de891a419c15
Rafael Vastag <rgvastag@gmail.com>
2021-11-16 13:42:46 -0300

    creating get all products method

---
+++ APICatalog/APICatalog/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using APICatalog.Context;
+using APICatalog.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalog.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly CatalogDbContext _context;
+
+        public ProductsController(CatalogDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> Get()
+        {
+           return _context.Products.ToList();
+        }
+    }
+}
