84f189883e3b0419b1bbb067bdf8c1203bce82ac
Rafael Vastag <rgvastag@gmail.com>
2021-11-18 14:19:31 -0300

    validation result -> interface IValidatableObject

--- APICatalog/APICatalog/Entities/Product.cs
+++ APICatalog/APICatalog/Entities/Product.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace APICatalog.Entities
 {
     [Table("RGV01_PRODUCTS")]
-    public class Product
+    public class Product : IValidatableObject
     {
         [Key]
         [Column("ID")]
@@ -27,7 +27,7 @@ namespace APICatalog.Entities
         public string Description { get; set; }
 
         [Required]
-        [Range(1,10000, ErrorMessage = "The price must be between {1} and {2}")]
+        [Range(1, 10000, ErrorMessage = "The price must be between {1} and {2}")]
         [Column("PRICE")]
         public decimal Price { get; set; }
 
@@ -36,11 +36,27 @@ namespace APICatalog.Entities
         [Column("IMAGE_URL")]
         public string ImagemUrl { get; set; }
 
-        public float  Inventory { get; set; }
+        public float Inventory { get; set; }
 
         public DateTime CreateDate { get; set; }
 
         public Category Category { get; set; }
         public int CategoryId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Name))
+            {
+                if (this.Name.ToString()[0].ToString() != this.Name.ToString()[0].ToString().ToUpper())
+                {
+                    yield return new ValidationResult("The attribute must be start with Upper letter", new[] { nameof(this.Name) });
+                }
+
+                if (this.Inventory <= 0)
+                {
+                    yield return new ValidationResult("Inventory must be greater than 0", new[] { nameof(this.Inventory) });
+                }
+            }
+        }
     }
 }
