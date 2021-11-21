a46eda43948ff25f6dbb011b90be4f359127d65a
Rafael Vastag <rgvastag@gmail.com>
2021-11-21 19:17:40 -0300

    Add middleware cors config

--- APICatalog/APICatalog/Startup.cs
+++ APICatalog/APICatalog/Startup.cs
@@ -33,6 +33,7 @@ namespace APICatalog
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
 
             var mappingConfig = new MapperConfiguration(mc =>
             {
@@ -99,6 +100,12 @@ namespace APICatalog
             app.UseAuthentication();
 
             app.UseAuthorization();
+
+            app.UseCors(options => 
+            options
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 
             app.UseEndpoints(endpoints =>
             {
